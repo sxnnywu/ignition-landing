a7aa6404ee017dc424ab3bddb4ae88380fe6ad41
Aryan Vasudevan <vasudevan.aryan@gmail.com>
2026-03-01 15:08:57 -0500

    Carousel working.

--- src/components/sections/Past.jsx
+++ src/components/sections/Past.jsx
@@ -1,22 +1,32 @@
+import { useState } from 'react';
 import "./Past.css";
 import leftArrow from '../../assets/illustrations/past/left-arrow.png';
 import rightArrow from '../../assets/illustrations/past/right-arrow.png';
-import whale from '../../assets/illustrations/past/whale.png'
-import team from '../../assets/illustrations/past/slideshow/team.png';
+import whale from '../../assets/illustrations/past/whale.png';
 import pastBg from '../../assets/backgrounds/past-bg.png';
+import team from '../../assets/illustrations/past/slideshow/team.png';
+import test1 from '../../assets/illustrations/past/slideshow/test1.png';
+import test2 from '../../assets/illustrations/past/slideshow/test2.jpg';
+
+const slides = [team, test1, test2];
 
 export default function Past() {
+    const [index, setIndex] = useState(0);
+
+    const prev = () => setIndex((i) => (i - 1 + slides.length) % slides.length);
+    const next = () => setIndex((i) => (i + 1) % slides.length);
+
     return (
         <section className="past-section" id="gallery">
             <div className="past-bg-wrapper">
                 <img src={pastBg} className="past-bg-image" alt="" />
                 <div className="slideshow">
-                    <img src={team} className="slideshow-image" />
+                    <img key={index} src={slides[index]} className="slideshow-image" />
                 </div>
 
-                <img src={leftArrow} className="left-arrow-illustration" />
-                <img src={rightArrow} className="right-arrow-illustration" />
-                <img src={whale} className="whale-illustration"/>
+                <img src={leftArrow} className="left-arrow-illustration" onClick={prev} />
+                <img src={rightArrow} className="right-arrow-illustration" onClick={next} />
+                <img src={whale} className="whale-illustration" />
             </div>
         </section>
     );
