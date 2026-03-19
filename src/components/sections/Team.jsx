3bacd8adbfb75c57487dcd13d1917b18f380b1c9
Sunny Wu <w.sunny0618@gmail.com>
2026-03-19 14:51:13 -0400

    Revert "Add team pictures"

--- src/components/sections/Team.jsx
+++ src/components/sections/Team.jsx
@@ -1,62 +1,10 @@
 import './Team.css';
-import teamBg from '../../assets/backgrounds/meet-the-team.png';
-
-import adam from '../../assets/headshots/adam.png';
-import anushka from '../../assets/headshots/anushka.png';
-import aryan from '../../assets/headshots/aryan.png';
-import ava from '../../assets/headshots/ava.png';
-import caden from '../../assets/headshots/caden.png';
-import chris from '../../assets/headshots/chris.png';
-import daisy from '../../assets/headshots/daisy.png';
-import emily from '../../assets/headshots/emily.png';
-import erin from '../../assets/headshots/erin.png';
-import haardik from '../../assets/headshots/haardik.png';
-import harshpreet from '../../assets/headshots/harshpreet.png';
-import ivan from '../../assets/headshots/ivan.png';
-import jamie from '../../assets/headshots/jamie.png';
-import jeremy from '../../assets/headshots/jeremy.png';
-import lily from '../../assets/headshots/lily.png';
-import nahyun from '../../assets/headshots/nahyun.png';
-import philip from '../../assets/headshots/philip.png';
-import sanidhya from '../../assets/headshots/sanidhya.png';
-import sebastian from '../../assets/headshots/sebastian.png';
-import shinena from '../../assets/headshots/shinena.png';
-import sunny from '../../assets/headshots/sunny.png';
-import thanusshan from '../../assets/headshots/thanusshan.png';
-import vishal from '../../assets/headshots/vishal.png';
-import winston from '../../assets/headshots/winston.png';
-import youssef from '../../assets/headshots/youssef.png';
+import teamBg from "../../assets/backgrounds/meet-the-team.png"
 
 export default function MeetTheTeam() {
-  return (
-    <section className="team">
-      <img src={teamBg} alt="Meet the Team" className="team-bg" />
-
-      <img src={adam}        alt="Adam"        className="headshot hs-adam" />
-      <img src={anushka}     alt="Anushka"     className="headshot hs-anushka" />
-      <img src={aryan}       alt="Aryan"       className="headshot hs-aryan" />
-      <img src={ava}         alt="Ava"         className="headshot hs-ava" />
-      <img src={caden}       alt="Caden"       className="headshot hs-caden" />
-      {/* <img src={chris}       alt="Chris"       className="headshot hs-chris" /> */}
-      <img src={daisy}       alt="Daisy"       className="headshot hs-daisy" />
-      <img src={emily}       alt="Emily"       className="headshot hs-emily" />
-      <img src={erin}        alt="Erin"        className="headshot hs-erin" />
-      <img src={haardik}     alt="Haardik"     className="headshot hs-haardik" />
-      <img src={harshpreet}  alt="Harshpreet"  className="headshot hs-harshpreet" />
-      <img src={ivan}        alt="Ivan"        className="headshot hs-ivan" />
-      <img src={jamie}       alt="Jamie"       className="headshot hs-jamie" />
-      <img src={jeremy}      alt="Jeremy"      className="headshot hs-jeremy" />
-      <img src={lily}        alt="Lily"        className="headshot hs-lily" />
-      <img src={nahyun}      alt="Nahyun"      className="headshot hs-nahyun" />
-      <img src={philip}      alt="Philip"      className="headshot hs-philip" />
-      {/* <img src={sanidhya}    alt="Sanidhya"    className="headshot hs-sanidhya" /> */}
-      {/* <img src={sebastian}   alt="Sebastian"   className="headshot hs-sebastian" /> */}
-      <img src={shinena}     alt="Shinena"     className="headshot hs-shinena" />
-      <img src={sunny}       alt="Sunny"       className="headshot hs-sunny" />
-      <img src={thanusshan}  alt="Thanusshan"  className="headshot hs-thanusshan" />
-      <img src={vishal}      alt="Vishal"      className="headshot hs-vishal" />
-      {/* <img src={winston}     alt="Winston"     className="headshot hs-winston" /> */}
-      <img src={youssef}     alt="Youssef"     className="headshot hs-youssef" />
-    </section>
-  );
+    return (
+        <section className="team">
+            <img src={teamBg} alt="Meet the Team" className="team-bg" />
+        </section>
+    );
 }
